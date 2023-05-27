b5fd25efd226e5c0a6402e2210560ddf3c62579b
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-27 11:02:47 +0200

    update

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,13 +1,25 @@
-
 // https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_VhTrCGP65dIGzY8rmbUl7pgkAFfxbHetBgABGPGnLaQCFWyU5T7IvvATQNKs3Uhk
 
-
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const URL = 'https://api.thecatapi.com/v1/breeds';
 const API_KEY = 'live_VhTrCGP65dIGzY8rmbUl7pgkAFfxbHetBgABGPGnLaQCFWyU5T7IvvATQNKs3Uhk';
 
-async function fetchBreeds(query) {
-   const res = await fetch(`${URL}?api_key=${API_KEY}&q=${query}`);
-   return await res.json();
-}
 
-export { fetchBreeds }; 
+export function fetchBreeds() {
+  return fetch(URL, {
+    headers: {
+      'x-api-key': API_KEY
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch breeds');
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data.map(breed => ({
+        id: breed.id,
+        name: breed.name
+      }));
+    });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,70 @@
 import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
 
-import SlimSelect from 'slim-select'
 new SlimSelect({
-  select: '#selectElement'
-})
+  select: '#selectElement',
+});
 
-import { fetchBreeds } from "./cat-api.js";
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+ 
+const refs = {
+  breedSelect: document.querySelector('.breed-select'),
+  loadingElement: document.querySelector('.loading'),
+  errorElement: document.querySelector('.error'),
+  catInfoContainer: document.querySelector('.cat-info'),
+};
 
-fetchBreeds().then((result) => console.log(result));
+// Обработчик события при выборе опции в селекте
+refs.breedSelect.addEventListener('change', function () {
+  const selectedBreedId = this.value;
+
+  fetchCatByBreed(selectedBreedId)
+    .then(catInfo => {
+      // Обновление информации о коте в DOM
+      const catImage = document.createElement('img');
+      catImage.src = catInfo.imageUrl;
+
+      const breedName = document.createElement('p');
+      breedName.textContent = `Breed: ${catInfo.breedName}`;
+
+      const description = document.createElement('p');
+      description.textContent = `Description: ${catInfo.description}`;
+
+      const temperament = document.createElement('p');
+      temperament.textContent = `Temperament: ${catInfo.temperament}`;
+
+      refs.catInfoContainer.innerHTML = '';
+      refs.catInfoContainer.appendChild(catImage);
+      refs.catInfoContainer.appendChild(breedName);
+      refs.catInfoContainer.appendChild(description);
+      refs.catInfoContainer.appendChild(temperament);
+
+      // Показ блока с информацией о коте
+      refs.catInfoContainer.style.display = 'block';
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+// Получение и заполнение списка пород при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        refs.breedSelect.appendChild(option);
+      });
+
+      refs.breedSelect.disabled = false;
+      refs.loadingElement.style.display = 'none';
+    })
+    .catch(error => {
+      console.error(error);
+      refs.errorElement.style.display = 'block';
+      refs.loadingElement.style.display = 'none';
+    });
+});
+
