701980d016ebf1a77527fb6fec587028a03f9862
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-28 13:08:57 +0200

    add commnets

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Импорт необходимых модулей
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import * as catApi from './cat-api.js';
@@ -9,6 +10,7 @@ const errorElement = document.querySelector('.error');
 const catInfoContainer = document.querySelector('.cat-info');
 const loaderElement = document.querySelector('.loader');
 
+// Обработчик события загрузки контента страницы
 document.addEventListener('DOMContentLoaded', () => {
   // Инициализация SlimSelect для выбора породы
   new SlimSelect({
@@ -20,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const getCatInfoByBreed = (selectedBreedId) => {
     return catApi.fetchCatByBreed(selectedBreedId)
       .then((catInfo) => {
+        // Формирование HTML с информацией о кошке
         const catInfoHTML = `
           <img src="${catInfo.imageUrl}">
           <p>Breed: ${catInfo.breedName}</p>
@@ -27,12 +30,17 @@ document.addEventListener('DOMContentLoaded', () => {
           <p>Temperament: ${catInfo.temperament}</p>
         `;
 
+        // Вставка HTML с информацией о кошке в контейнер и отображение контейнера
         catInfoContainer.innerHTML = catInfoHTML;
         catInfoContainer.style.display = 'block';
+
+        // Скрытие элемента загрузки
         loaderElement.classList.remove('visible');
       })
       .catch((error) => {
         console.error(error);
+
+        // Скрытие элемента загрузки и вывод уведомления об ошибке
         loaderElement.classList.remove('visible');
         Notiflix.Notify.failure('Произошла ошибка. Пожалуйста, повторите попытку позже.');
       });
@@ -42,8 +50,10 @@ document.addEventListener('DOMContentLoaded', () => {
   breedSelect.addEventListener('change', () => {
     const selectedBreedId = breedSelect.value;
 
+    // Отображение элемента загрузки
     loaderElement.classList.add('visible');
 
+    // Получение информации о кошке по выбранной породе
     getCatInfoByBreed(selectedBreedId);
   });
 
@@ -51,17 +61,21 @@ document.addEventListener('DOMContentLoaded', () => {
   catApi.fetchBreeds()
     .then((breeds) => {
       breeds.forEach((breed) => {
+        // Создание и добавление элемента <option> в выбор породы
         const option = document.createElement('option');
         option.value = breed.id;
         option.textContent = breed.name;
         breedSelect.appendChild(option);
       });
 
-      breedSelect.disabled = false; // Включение выбора породы
-      loadingElement.style.display = 'none'; // Скрытие элемента загрузки
+      // Включение выбора породы и скрытие элемента загрузки после успешной загрузки списка пород
+      breedSelect.disabled = false;
+      loadingElement.style.display = 'none';
     })
     .catch((error) => {
       console.error(error);
+
+      // Вывод уведомления об ошибке и скрытие элемента загрузки в случае ошибки при загрузке списка пород
       Notiflix.Notify.failure('Не удалось загрузить список пород. Пожалуйста, повторите попытку позже.');
       loadingElement.style.display = 'none';
     });
