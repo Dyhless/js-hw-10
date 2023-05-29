999081bfe7802be39de43254c74d0e379ae029ba
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-29 21:05:16 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ const errorElement = document.querySelector('.error');
 const catInfoContainer = document.querySelector('.cat-info');
 const loaderElement = document.querySelector('.loader');
 
+loadingElement.style.display = 'none';
 errorElement.style.display = 'none';
 // Обработчик события загрузки контента страницы
 document.addEventListener('DOMContentLoaded', () => {
